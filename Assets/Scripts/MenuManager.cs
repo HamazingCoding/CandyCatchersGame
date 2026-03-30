a3dc318c348d2f2488ad1420da9595f242090b9a
HamazingCoding <hamazing174@gmail.com>
2026-03-30 15:12:37 +0400

    Added Settings Page

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -27,4 +27,8 @@ public class MenuManager : MonoBehaviour
     {
         SceneManager.LoadScene("LeaderboardScene");
     }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
 }
